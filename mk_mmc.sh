0ec50aefcf241089967c589461c03be99719df46
Robert Nelson <robertcnelson@gmail.com>
2010-04-13 19:13:14 -0500

    initial flasher release

---
+++ mk_mmc.sh
@@ -0,0 +1,157 @@
+#!/bin/bash -e
+
+#Notes: need to check for: parted, fdisk, wget, mkfs.*, mkimage, md5sum
+
+MIRROR="http://rcn-ee.net/deb/tools/"
+MLO="MLO-beagleboard-1.44+r9+gitr1c9276af4d6a5b7014a7630a1abeddf3b3177563-r9"
+XLOAD="x-load-beagleboard-1.44+r9+gitr1c9276af4d6a5b7014a7630a1abeddf3b3177563-r9.bin.ift"
+UBOOT="u-boot-beagleboard-2010.03-rc1+r44+gitr946351081bd14e8bf5816fc38b82e004a0e6b4fe-r44.bin"
+
+unset MMC
+
+BOOT_LABEL=boot
+
+DIR=$PWD
+
+function dl_xload_uboot {
+ mkdir -p ${DIR}/dl/
+
+ echo ""
+ echo "Downloading X-loader and Uboot"
+ echo ""
+
+ wget -c --no-verbose --directory-prefix=${DIR}/dl/ ${MIRROR}${MLO}
+ wget -c --no-verbose --directory-prefix=${DIR}/dl/ ${MIRROR}${XLOAD}
+ wget -c --no-verbose --directory-prefix=${DIR}/dl/ ${MIRROR}${UBOOT}
+}
+
+function cleanup_sd {
+
+ echo ""
+ echo "Umounting Partitions"
+ echo ""
+
+ sudo umount ${MMC}1 &> /dev/null || true
+ sudo umount ${MMC}2 &> /dev/null || true
+
+ sudo parted -s ${MMC} mklabel msdos
+}
+
+function create_partitions {
+
+sudo fdisk -H 255 -S 63 ${MMC} << END
+n
+p
+1
+1
++64M
+a
+1
+t
+e
+p
+w
+END
+
+echo ""
+echo "Formating Boot Partition"
+echo ""
+
+sudo mkfs.vfat -F 16 ${MMC}1 -n ${BOOT_LABEL}
+
+sudo rm -rfd ./disk || true
+
+mkdir ./disk
+sudo mount ${MMC}1 ./disk
+
+sudo cp -v ${DIR}/dl/${MLO} ./disk/MLO
+sudo cp -v ${DIR}/dl/${XLOAD} ./disk/x-load.bin.ift
+sudo cp -v ${DIR}/dl/${UBOOT} ./disk/u-boot.bin
+sudo mkimage -A arm -O linux -T script -C none -a 0 -e 0 -n "Reset NAND" -d ./reset.cmd ./disk/boot.scr
+
+cd ./disk
+sync
+cd ..
+sudo umount ./disk || true
+echo "done"
+
+}
+
+function check_mmc {
+ FDISK=$(sudo fdisk -l | grep "Disk ${MMC}" | awk '{print $2}')
+
+ if test "-$FDISK-" = "-$MMC:-"
+ then
+  echo ""
+  echo "I see..."
+  echo "sudo fdisk -l:"
+  sudo fdisk -l | grep "Disk /dev/" --color=never
+  echo ""
+  echo "mount:"
+  mount | grep -v none | grep "/dev/" --color=never
+  echo ""
+  read -p "Are you 100% sure, on selecting [${MMC}] (y/n)? "
+  [ "$REPLY" == "y" ] || exit
+  echo ""
+ else
+  echo ""
+  echo "Are you sure? I Don't see [${MMC}], here is what I do see..."
+  echo ""
+  echo "sudo fdisk -l:"
+  sudo fdisk -l | grep "Disk /dev/" --color=never
+  echo ""
+  echo "mount:"
+  mount | grep -v none | grep "/dev/" --color=never
+  echo ""
+  exit
+ fi
+}
+
+function usage {
+    echo "usage: $(basename $0) --mmc /dev/sdd"
+cat <<EOF
+
+required options:
+--mmc </dev/sdX>
+    Unformated MMC Card
+
+Additional/Optional options:
+-h --help
+    this help
+EOF
+exit
+}
+
+function checkparm {
+    if [ "$(echo $1|grep ^'\-')" ];then
+        echo "E: Need an argument"
+        usage
+    fi
+}
+
+# parse commandline options
+while [ ! -z "$1" ]; do
+    case $1 in
+        -h|--help)
+            usage
+            MMC=1
+            ;;
+        --mmc)
+            checkparm $2
+            MMC="$2"
+            check_mmc 
+            ;;
+    esac
+    shift
+done
+
+if [ ! "${MMC}" ];then
+    usage
+fi
+
+ dl_xload_uboot
+ cleanup_sd
+ create_partitions
+
+
+
