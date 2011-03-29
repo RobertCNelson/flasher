f3e9b6d8decdb86805f59982903b70e7ab5a3a28
Robert Nelson <robertcnelson@gmail.com>
2011-03-29 12:05:41 -0500

    boot.scr to uEnv.txt compatibility
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- mk_mmc.sh
+++ mk_mmc.sh
@@ -169,6 +169,7 @@ sudo mount ${MMC}${PARTITION_PREFIX}1 ${TEMPDIR}/disk
 sudo cp -v ${TEMPDIR}/dl/${MLO} ${TEMPDIR}/disk/MLO
 sudo cp -v ${TEMPDIR}/dl/${UBOOT} ${TEMPDIR}/disk/u-boot.bin
 sudo mkimage -A arm -O linux -T script -C none -a 0 -e 0 -n "Reset NAND" -d ${DIR}/reset.cmd ${TEMPDIR}/disk/user.scr
+sudo cp -v ${DIR}/uEnv.txt ${TEMPDIR}/disk/user.txt
 
 cd ${TEMPDIR}/disk
 sync
