1773adc1b07c5c5be7f5e9d658613cbd68eb3afd
Robert Nelson <robertcnelson@gmail.com>
2012-04-19 10:01:35 -0500

    and we had to bring back mkimage, as it uses a raw script, need to rewrite uEnv.txt
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ reset.cmd
@@ -0,0 +1,17 @@
+echo "Starting NAND UPGRADE, do not REMOVE SD CARD or POWER till Complete"
+fatload mmc 0:1 0x80200000 MLO
+nandecc hw
+nand erase 0 80000
+nand write 0x80200000 0 20000
+nand write 0x80200000 20000 20000
+nand write 0x80200000 40000 20000
+nand write 0x80200000 60000 20000
+
+fatload mmc 0:1 0x80200000 u-boot.img
+nandecc hw
+nand erase 80000 160000
+nand write 0x80200000 80000 170000
+nand erase 260000 20000
+echo "FLASH UPGRADE Complete"
+exit
+
